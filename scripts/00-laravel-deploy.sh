ea638c686536ecd3e5a1797b24f241bdd1490e9d
rizkyromadhon <rizkyrr1123@gmail.com>
2025-05-24 22:41:51 +0700

    Build

---
+++ scripts/00-laravel-deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+echo "Running composer"
+composer install --no-dev --working-dir=/var/www/html
+
+echo "Clearing caches..."
+php artisan optimize:clear
+
+echo "Caching config..."
+php artisan config:cache
+
+echo "Caching routes..."
+php artisan route:cache
+
+echo "Running migrations..."
+php artisan migrate --force
+
+echo "done deploying"
